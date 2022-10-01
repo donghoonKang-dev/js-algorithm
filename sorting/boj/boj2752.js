5e56a509642005137a781d1ef169cd831230e58e
donghoonKang-dev <donghoon.dev@gmail.com>
2022-10-01 15:30:27 +0900

    boj2752.js

---
+++ sorting/boj/boj2752.js
@@ -0,0 +1,26 @@
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input;
+
+readline
+  .on("line", function (line) {
+    input = line;
+    readline.close();
+  })
+  .on("close", function () {
+    solution(input);
+    process.exit();
+  });
+
+const solution = (input) => {
+  console.log(
+    input
+      .split(" ")
+      .map((v) => parseInt(v))
+      .sort((a, b) => a - b)
+      .join(" ")
+  );
+};
