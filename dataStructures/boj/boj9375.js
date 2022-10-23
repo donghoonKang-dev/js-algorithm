f88d69e7bc630c1da86eaf0b742b7324d0a4366b
donghoonKang-dev <donghoon.dev@gmail.com>
2022-10-23 22:58:48 +0900

    boj9375.js 2

--- dataStructures/boj/boj9375.js
+++ dataStructures/boj/boj9375.js
@@ -11,13 +11,36 @@ readline
   })
   .on("close", function () {
     let testCase = parseInt(input[0]);
+    let inputPointer = 1;
+
     for (let i = 0; i < testCase; i++) {
-      let n = parseInt(input[3 * i + 1]);
-      solution(n, list);
+      let n = parseInt(input[inputPointer]);
+      let clothes = input
+        .slice(inputPointer + 1, inputPointer + 1 + n)
+        .map((cloth) => cloth.split(" "));
+
+      solution(n, clothes);
+      inputPointer = inputPointer + 1 + n;
     }
     process.exit();
   });
 
-const solution = (input) => {
-  //이 안에 솔루션 작성
+const solution = (n, clothes) => {
+  let result = 1;
+  let clothesDict = {}; //카테고리별 의상 배열을 담는 딕셔너리 형태
+
+  for (const cloth of clothes) {
+    let content = cloth[0];
+    let category = cloth[1];
+
+    if (Object.keys(clothesDict).includes(category))
+      clothesDict[category].push(content);
+    else clothesDict[category] = [content];
+  }
+
+  for (const clothCategory in clothesDict) {
+    result = result * (clothesDict[clothCategory].length + 1);
+  }
+
+  console.log(result - 1);
 };
