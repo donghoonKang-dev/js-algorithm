bff3dc83a1d6f6125b1535a37a7e2a34a3a53fe8
donghoonKang-dev <donghoon.dev@gmail.com>
2022-11-02 19:54:50 +0900

    boj3190x.js

---
+++ dataStructures/boj/boj3190x.js
@@ -0,0 +1,100 @@
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+readline
+  .on("line", function (line) {
+    input.push(line);
+  })
+  .on("close", function () {
+    //보드 초기 세팅
+    let N = parseInt(input[0]);
+    let board = Array.from(Array(N + 2), () => Array(N + 2).fill(0));
+
+    //벽 세팅
+    for (let i = 0; i < N + 1; i++) {
+      board[i][0] = 1;
+      board[0][i] = 1;
+      board[i][N + 1] = 1;
+      board[N + 1][i] = 1;
+    }
+
+    //사과세팅
+    let K = parseInt(input[1]);
+    let applePositions = input
+      .slice(2, 2 + K)
+      .map((item) => [
+        parseInt(item.split(" ")[0]),
+        parseInt(item.split(" ")[1]),
+      ]);
+    for (let i = 0; i < K; i++) {
+      let x = applePositions[i][0];
+      let y = applePositions[i][1];
+      board[x][y] = 2;
+    }
+
+    //뱀의 방향 변환 맵 생성
+    let L = parseInt(input[2 + K]);
+    const directions = new Map();
+    for (let i = 0; i < L; i++) {
+      directions.set(
+        parseInt(input[3 + K + i].split(" ")[0]),
+        input[3 + K + i].split(" ")[1]
+      );
+    }
+
+    const horizontal = [-1, 0, 1, 0];
+    const vertical = [0, 1, 0, -1];
+
+    let snakesDirection = 1;
+    let snake = [[1, 1]];
+    let time = 0;
+    let isEnd = false;
+
+    while (!isEnd) {
+      if (directions.has(time)) {
+        if (directions.get(time) === "D")
+          snakesDirection = (snakesDirection + 1) % 4;
+        else snakesDirection = (snakesDirection + 3) % 4;
+      }
+
+      time += 1;
+
+      let head =
+        board[snake[0][0] + horizontal[snakesDirection]][
+          snake[0][1] + vertical[snakesDirection]
+        ];
+
+      if (head === 1) break;
+      else {
+        snake.unshift([
+          snake[0][0] + horizontal[snakesDirection],
+          snake[0][1] + vertical[snakesDirection],
+        ]);
+        if (head === 2)
+          board[snake[0][0] + horizontal[snakesDirection]][
+            snake[0][1] + vertical[snakesDirection]
+          ] = 0;
+        else {
+          for (let i = snake.length - 1; i >= 1; i--) {
+            let [tmpx, tmpy] = snake[i];
+            if (
+              tmpx === snake[0][0] + horizontal[snakesDirection] &&
+              tmpy === snake[0][1] + vertical[snakesDirection]
+            ) {
+              isEnd = true;
+              break;
+            }
+          }
+          snake.pop();
+        }
+      }
+    }
+
+    console.log(time);
+
+    process.exit();
+  });
